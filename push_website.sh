ea486c0611bfb6b9af12aea01471f0c8ebb8236f
Mayank Sharan <mayanksharan96@gmail.com>
2022-08-21 23:28:07 -0700

    Added research project 1

--- push_website.sh
+++ push_website.sh
@@ -1,9 +1,10 @@
- & serverPID=$!
-sleep 2
+bundle exec jekyll serve & serverPID=$!
+sleep 30
 kill $serverPID
 cp -r _site/* ../sharanmayank.github.io/
 cd ../sharanmayank.github.io/
+echo "Preparing code push"
 git add *
-git commit -m $1
+git commit -m "$1"
 git push
 echo "Push Complete"
